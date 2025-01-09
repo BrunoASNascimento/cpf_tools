81b44f8bab4b22b242b5d93a1ba13b2ce84c0f1b
BrunoASNascimento <bruno-asn@outlook.com>
2025-01-09 15:44:03 -0300

    Add tests for CPF formatting and validation functions

--- test.py
+++ test.py
@@ -18,14 +18,11 @@ class TestCpf(unittest.TestCase):
         self.assertEqual(cpf_str_validation(cpf=1274711835), False)
 
     def test_cpf_format(self):
-        self.assertEqual(
-            cpf_format(cpf=46374367880.0), '463.743.678-80')
-        self.assertEqual(
-            cpf_format(cpf=46374367880), '463.743.678-80')
-        self.assertEqual(
-            cpf_format(cpf=4.637437e+10), '463.743.700-00')
-        self.assertEqual(
-            cpf_format(cpf='5*7das4.71dads1.8-35'), '005.747.118-35')
+        self.assertEqual(cpf_format(cpf=46374367880.0), '463.743.678-80')
+        self.assertEqual(cpf_format(cpf=46374367880), '463.743.678-80')
+        self.assertEqual(cpf_format(cpf=4.637437e+10), '463.743.700-00')
+        self.assertEqual(cpf_format(
+            cpf='5*7das4.71dads1.8-35'), '005.747.118-35')
 
 
 if __name__ == "__main__":
---
+++ tests/test_cpf_tools.py
@@ -0,0 +1,69 @@
+from cpf_tools import cpf_int_validation, cpf_str_validation, cpf_format
+
+
+class TestClass:
+    def test_when_int_number_input_cpf_format(self):
+        # Given
+        input_value = 46374367880
+        output_value = '463.743.678-80'
+
+        # When
+        result = cpf_format(input_value)
+
+        # Then
+        assert result == output_value
+
+    def test_when_str_input_cpf_format(self):
+        # Given
+        input_value = '5*7das4.71dads1.8-35'
+        output_value = '005.747.118-35'
+
+        # When
+        result = cpf_format(input_value)
+
+        # Then
+        assert result == output_value
+        
+    def test_when_str_input_cpf_format(self):
+        # Given
+        input_value = '5*7das4.71dads1.8-35'
+        output_value = '005.747.118-35'
+
+        # When
+        result = cpf_format(input_value)
+
+        # Then
+        assert result == output_value
+
+    def test_when_str_input_cpf_format(self):
+        # Given
+        input_value = '5*7das4.71dads1.8-35'
+        output_value = '005.747.118-35'
+
+        # When
+        result = cpf_format(input_value)
+
+        # Then
+        assert result == output_value
+
+    def test_when_str_input_cpf_format(self):
+        # Given
+        input_value = '5*7das4.71dads1.8-35'
+        output_value = '005.747.118-35'
+
+        # When
+        result = cpf_format(input_value)
+
+        # Then
+        assert result == output_value
+    
+    def test_when_str_input_cpf_format(self):
+        # Given
+        input_value = '5*7das4.71dads1.8-35'
+        output_value = '005.747.118-35'
+
+        # When
+        result = cpf_format(input_value)
+
+        # Then
+        assert result == output_value
